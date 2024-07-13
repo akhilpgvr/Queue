09aa90f9c4fcdafb202fbda32710caf4aaa4975b
Akhil <akhilpgvr@gmail.com>
2024-07-14 01:11:26 +0530

    added 1. Priority Queue 2. LinkedList Queue 3. PriorityBlockingQueue

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class Main {
     public static void main(String[] args) {
+
+        Queue<Integer> priorityQueue = new PriorityQueue<>(); // based on priority, but not thread safe
+        Queue<String> linkedListQueue = new LinkedList<>(); // normal queue with FIFO rule, but not thread safe
+        Queue<Integer> priorityBlockingQueue = new PriorityBlockingQueue<>(); // based on priority and is thread safe
         System.out.println("..........................");
+        priorityQueue.add(7);
+        priorityQueue.add(3);
+        priorityQueue.add(5);
+        while(!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll());
+        }
+        System.out.println("..........................");
+        linkedListQueue.add("e");
+        linkedListQueue.add("b");
+        linkedListQueue.add("d");
+        linkedListQueue.add("a");
+        while(!linkedListQueue.isEmpty()) {
+            System.out.println(linkedListQueue.poll());
+        }
+        System.out.println("..........................");
+        priorityBlockingQueue.add(10);
+        priorityBlockingQueue.add(8);
+        priorityBlockingQueue.add(9);
+        System.out.println(priorityBlockingQueue.poll());
 
     }
 }
